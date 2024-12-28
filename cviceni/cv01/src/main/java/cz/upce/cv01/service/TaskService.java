d4bed015a47e16e84e6c2812d27d4b68ae3f5fe9
Ondřej Matějka <tejkys@email.cz>
2024-12-28 16:42:11 +0100

    init commit

---
+++ cviceni/cv01/src/main/java/cz/upce/cv01/service/TaskService.java
@@ -0,0 +1,18 @@
+package cz.upce.cv01.service;
+
+import cz.upce.cv01.domain.Task;
+import cz.upce.cv01.repository.TaskRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public List<Task> findAllByAppUserId(final Long appUserId) {
+        return taskRepository.findAllByAuthorId(appUserId);
+    }
+}
